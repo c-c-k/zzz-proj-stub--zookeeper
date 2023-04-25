852d70867bc44846ae228c6c436f15ea1afc437f
c-c-k <c-c-k@nym.hush.com>
2023-04-25 18:18:28 +0300

    stage 4 done

--- tests/test_zookeeper.py
+++ tests/test_zookeeper.py
@@ -7,27 +7,27 @@ from zookeeper import zookeeper, messages
 
 
 @pytest.mark.parametrize(
-    ('habitat_id', 'animal'),
+    ('commands', 'output'),
     (
-        ('0', 'camel '),
-        ('1', 'lion '),
-        ('2', 'deer '),
-        ('3', 'goose '),
-        ('4', 'bat '),
-        ('5', 'rabbit '),
-        ('-1', 'exit'),
-        ('text input\n-1', 'number'),
-        ('999\n-1', 'not found'),
-        ('text input\n-1', '-1'),
-        ('999\n-1', '-1'),
+        ([], ['later']),
+        (['0'], ['camel']),
+        (['1'], ['lion']),
+        (['2'], ['deer']),
+        (['3'], ['goose']),
+        (['4'], ['bat']),
+        (['5'], ['rabbit']),
+        (['bad input'], ['number']),
+        (['999'], ['not found']),
+        (['1', '5'], ['lion', 'rabbit']),
     )
 )
-def test_main_output(capsys, monkeypatch, habitat_id, animal):
-    stdin_override = StringIO(initial_value=habitat_id)
+def test_main_output(capsys, monkeypatch, commands, output):
+    stdin_override = StringIO(initial_value='\n'.join((*commands, 'exit')))
     with monkeypatch.context() as m:
         m.setattr(sys, 'stdin', stdin_override)
         zookeeper.main()
     captured = capsys.readouterr()
-    assert animal in captured.out.strip()
     assert 'Please enter' in captured.out.strip()
-    assert 'restart' in captured.out.strip()
+    assert 'exit' in captured.out.strip()
+    for string in output:
+        assert string in captured.out.strip()
--- zookeeper/zookeeper.py
+++ zookeeper/zookeeper.py
@@ -7,23 +7,21 @@ except ImportError:
 def main():
     print('Please enter the number of the habitat '
           'you would like to view:')
+    print('enter "exit" to exit the Zoo Keeper.')
     while True:
+        command = input()
+        if command == 'exit':
+            break
         try:
-            habitat_id = int(input())
+            habitat_id = int(command)
         except ValueError:
             print('habitat id must be a number, please try again.')
         else:
             if 0 <= habitat_id < len(HABITATS):
                 print(HABITATS[habitat_id])
-                break
-            elif habitat_id == -1:
-                print('exiting, have a good day')
-                break
             else:
                 print('habitat id not found, please try again')
-        print('enter "-1" to exit')
-    print("---\nYou've reached the end of the program. "
-          'To check another habitat, please restart the watcher.')
+    print('See you later!')
 
 
 if __name__ == "__main__":
