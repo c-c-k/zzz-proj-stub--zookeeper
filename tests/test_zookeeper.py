a0fd8f5c0a29607fadd28d1004b212d72a9952c7
c-c-k <c-c-k@nym.hush.com>
2023-04-25 18:18:28 +0300

    stage 3 done

--- tests/test_zookeeper.py
+++ tests/test_zookeeper.py
@@ -1,7 +1,33 @@
+from io import StringIO
+import sys
+
+import pytest
+
 from zookeeper import zookeeper, messages
 
 
-def test_main_output(capsys):
-    zookeeper.main()
+@pytest.mark.parametrize(
+    ('habitat_id', 'animal'),
+    (
+        ('0', 'camel '),
+        ('1', 'lion '),
+        ('2', 'deer '),
+        ('3', 'goose '),
+        ('4', 'bat '),
+        ('5', 'rabbit '),
+        ('-1', 'exit'),
+        ('text input\n-1', 'number'),
+        ('999\n-1', 'not found'),
+        ('text input\n-1', '-1'),
+        ('999\n-1', '-1'),
+    )
+)
+def test_main_output(capsys, monkeypatch, habitat_id, animal):
+    stdin_override = StringIO(initial_value=habitat_id)
+    with monkeypatch.context() as m:
+        m.setattr(sys, 'stdin', stdin_override)
+        zookeeper.main()
     captured = capsys.readouterr()
-    assert captured.out.strip() == messages.CAMEL.strip()
+    assert animal in captured.out.strip()
+    assert 'Please enter' in captured.out.strip()
+    assert 'restart' in captured.out.strip()
--- zookeeper/zookeeper.py
+++ zookeeper/zookeeper.py
@@ -1,11 +1,29 @@
 try: 
-    from . import messages
+    from .messages import HABITATS
 except ImportError:
-    import messages
+    from messages import HABITATS
 
 
 def main():
-    print(messages.CAMEL)
+    print('Please enter the number of the habitat '
+          'you would like to view:')
+    while True:
+        try:
+            habitat_id = int(input())
+        except ValueError:
+            print('habitat id must be a number, please try again.')
+        else:
+            if 0 <= habitat_id < len(HABITATS):
+                print(HABITATS[habitat_id])
+                break
+            elif habitat_id == -1:
+                print('exiting, have a good day')
+                break
+            else:
+                print('habitat id not found, please try again')
+        print('enter "-1" to exit')
+    print("---\nYou've reached the end of the program. "
+          'To check another habitat, please restart the watcher.')
 
 
 if __name__ == "__main__":
